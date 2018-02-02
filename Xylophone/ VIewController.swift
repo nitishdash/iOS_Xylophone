b5c350690ddfd1375d3c1be35f4d3b11a8beff29
Nitish Dash <nitishdash@Nitishs-MacBook-Pro.local>
2018-02-02 10:50:32 +0530

    Init commit

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController{
     
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +21,28 @@ class ViewController: UIViewController{
 
     @IBAction func notePressed(_ sender: UIButton) {
         
+
+        let url = noteProvider(withKey: sender.tag)
         
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            guard let player = player else {
+                return
+            }
+            player.prepareToPlay()
+            player.play()
+        }
+        catch let error as Error    {
+            print(error)
+        }
+        
+    }
+    
+    func noteProvider(withKey keySent:Int) -> URL {
+        
+    let url = Bundle.main.url(forResource: "note"+"\(keySent)", withExtension: "wav")
+    
+    return url!
         
     }
     
